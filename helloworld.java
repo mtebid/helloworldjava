27f79d86fae30098f143a45c65bd557af5bab38c
mtebid <105900722+mtebid@users.noreply.github.com>
2022-06-11 15:17:34 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello World 11th june - 3:15PM - webhooks!");
+    System.out.println("Hello World 11th june - 3:15PM , Yeshua , have mercy on me - webhooks!");
     }
 }
