0c22cae27fc70c999ba2ce7953f2187135fba289
mtebid <105900722+mtebid@users.noreply.github.com>
2022-06-13 13:46:30 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello World 11th june - 3:15PM to 3:35 , Yeshua , have mercy on your namesake, yes papa !");
+    System.out.println("Hello World 11th june - 3:15PM to 3:35 , Yeshua hamashiya the same yesterday today and forever !");
     }
 }
